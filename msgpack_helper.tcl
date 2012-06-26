7ab47dfde3021ad741f53220505073ee18643a46
jdc8 <jos.decoster@gmail.com>
2012-06-26 04:30:59 -0400

    map2dict and map2array helper functions

--- msgpack_helper.tcl
+++ msgpack_helper.tcl
@@ -1,4 +1,27 @@
 namespace eval ::msgpack {
     namespace export *
     namespace ensemble create
+
+    proc map2dict {m} {
+	set d [dict create]
+	foreach {ki vi} $m {
+	    lassign $ki kt kv
+	    lassign $vi vt vv
+	    dict set d $kv $vv
+	}
+	return $d
+    }
+
+    proc map2array {m anm} {
+	upvar $anm a
+	foreach {ki vi} $m {
+	    lassign $ki kt kv
+	    lassign $vi vt vv
+	    set a($kv) $vv
+	}
+    }
+
+    proc array2list {a} {
+    }
 }
+
